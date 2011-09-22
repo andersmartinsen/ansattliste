9ecf1966e092eb48d638af66ca5787d546cb8881
Anders Martinsen <anders.martinsen@bekk.no>
2011-09-22 18:57:01 +0200

    Lagt til fasit fra workshopen.

--- www/javascript/app.js
+++ www/javascript/app.js
@@ -1,40 +1,98 @@
-// Lag et script som kjøres når siden lastes vha. JQuery
-// Lag et passende javascriptobjekt som kan holde på variablene (eks. ansatte i et JSON-objekt) og metodene du trenger
-// Når siden er lastet (ready), bruker du $.ajax() til å hente alle ansatte som JSON fra /employees
-// Bruk en mustache template til å generere liste-element-htmlen du trenger
-// Bruk JQuery til å sette disse elementene inn i ansattlisten du lagde i index.html
-
-// Lag en funksjon som kan vise en enkelt ansatt
-// Denne må ha en template som inkluderer alt JQuery mobile forventer av en side
-// Denne siden må nå legges til $.mobile.pageContainer
-// Deretter må du hente den nye siden med changePage()
-
-// Lag en funksjon som filtrerer ansattlisten din på avdeling (tech., funk osv.)
-// Lag en nav-struktur i index.html som bytter mellom avdelingene
-// Lytt etter click-event på nav-knappene og kall filter-funksjonen din
-// Denne må så tegne ansattlisten på nytt
-
 $(function(){
-	window.EmployeeApp = {
+	window.App = {
 		employees: null,
 		filter: null,
 		department: "Alle",
 		allFilter: function(employee){return true;},
 		belongsToDepartmentFilter: function(employee){
-			// Sett inn kode
+			return employee['Department'] == App.department;
 		},
 		showEmployee: function(id){
-			// Sett inn kode
+			var employee = _.detect(this.employees, function(emp){
+				return emp['Id'] == id;
+			});
+			var template = 	'<div data-role=page data-url=employee/{{Id}}>' + 
+            '<div data-role=header>' +
+            '<h1>{{FirstName}} {{LastName}}' +
+            '</div>' +
+            '<div data-role=content>' +
+            '<img src="{{ImageUrl}}"/>' +
+            '<br /><br />' +
+            '<table>' +
+            '<tr>' +
+            '<th scope="row">Navn:</th>' +
+            '<td>{{FirstName}} {{LastName}}</td>' + 
+            '</tr>' +
+            '<tr>' +
+            '<th scope="row">Stilling:</th>' +
+            '<td>{{Seniority}}</td>' + 
+            '</tr>' +
+            '<tr>' +
+            '<th scope="row">Avdeling:</th>' +
+            '<td>{{InterestGroup}}</td>' + 
+            '</tr>' +
+            '<tr>' +
+            '<th scope="row">Epost:</th>' +
+            '<td>{{Email}}</td>' + 
+            '</tr>' +
+            '<tr>' +
+            '<th scope="row">Telefon:</th>' +
+            '<td>{{MobilePhone}}</td>' + 
+            '</tr>' +
+            '<tr>' +
+            '<th scope="row">Adresse:</th>' +
+            '<td>{{StreetAddress}}, {{PostalNr}} {{PostalAddress}}</td>' + 
+            '</tr>' +
+            '</table>' +
+            '</div>' + 
+            '</div>';
+			var html = Mustache.to_html(template, employee);
+			var newPage = $(html);
+			newPage.appendTo($.mobile.pageContainer);
+			$.mobile.changePage(newPage);
 		},
-		renderList: function(data){
-			// Sett inn kode
+		render: function(data){
+			var template = 	'<li><a href="" onClick="App.showEmployee({{Id}}); return false;">' +
+            '<img src="{{ImageUrl}}"/>' +
+            '<h3>{{FirstName}} {{LastName}}</h3>' +
+            '<p>{{Email}}</p></a></li>';
+			var html = _.reduce(_.filter(data, this.filter), function(result, employee) {
+				return result + Mustache.to_html(template, employee);
+			}, "");
+			$('#employees').html(html).listview('refresh');
 		},
 		init: function(){
-			// Sett inn kode
+			this.filter = this.allFilter;
+			$.ajax({
+				url: '/employees',
+				contentType: "application/json; charset=utf-8",
+				dataType: "json",
+				error: function(){
+					alert("error");
+				},
+				success: function(data){
+					App.employees = data;
+					App.render(data);
+				}
+			});
 		}
 	}
-	$('.nav-button').click(function(){
-		// Sett inn kode
+	$('.my-nav').click(function(){
+		$('.ui-btn-active').removeClass('ui-btn-active');
+		$(this).addClass('ui-btn-active');
+		var $target = $(this).children();
+		while( $target.length ) {
+            $target = $target.children();
+		}
+		var department = $target.end().text();
+		if (department == "Alle"){
+			App.filter = App.allFilter;
+		} else {
+			App.department = department;
+			App.filter = App.belongsToDepartmentFilter;
+		}
+		App.render(App.employees);
+		return false;
 	});
-	EmployeeApp.init();
+	App.init();
 });
